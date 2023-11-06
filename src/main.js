e2674847f38e3f11d65d97518d4bb383185485f7
ugurakcora <uakcora97@gmail.com>
2023-11-06 17:28:45 +0300

    added includes folder

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VeeValidatePlugin from './includes/validation'
 
 import App from './App.vue'
 import router from './router'
@@ -11,5 +12,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VeeValidatePlugin)
 
 app.mount('#app')
