7d9ae93b1b0dd8d6e23d44621e1ef2a6b5dbd3d2
ugurakcora <uakcora97@gmail.com>
2023-11-06 14:59:05 +0300

    updated modal

--- src/stores/modal.js
+++ src/stores/modal.js
@@ -2,6 +2,11 @@ import { defineStore } from 'pinia'
 
 export default defineStore('modal', {
   state: () => ({
-    isOpen: false,
-  })
+    isOpen: false
+  }),
+  getters: {
+    hiddenClass(state) {
+      return !state.isOpen ? 'hidden' : ''
+    }
+  }
 })
