abd89f87f455e4d7cf3ba769e570ecc4ae248505
ugurakcora <uakcora97@gmail.com>
2023-11-10 00:50:13 +0300

    added actions

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -99,8 +99,7 @@
 </template>
 
 <script>
-import { auth, usersCollection } from '@/includes/firebase'
-import { mapWritableState } from 'pinia'
+import { mapActions } from 'pinia'
 import useUserStore from '@/stores/user'
 export default {
   name: 'RegisterForm',
@@ -124,10 +123,10 @@ export default {
       reg_alert_msg: 'Please wait! Your account is being created.'
     }
   },
-  computed: {
-    ...mapWritableState(useUserStore(), ['userLoggedIn'])
-  },
   methods: {
+    ...mapActions(useUserStore(), {
+      createUser: 'register'
+    }),
     async register(values) {
       this.reg_show_alert = true
       this.reg_in_submission = true
@@ -136,30 +135,13 @@ export default {
 
       let userCred = null
       try {
-        userCred = await auth.createUserWithEmailAndPassword(values.email, values.password)
+        await this.createUser(values)
       } catch (error) {
         this.reg_in_submission = false
         this.reg_alert_variant = 'bg-red-500'
         this.reg_alert_msg = 'An unexpected error occurred. Please try again later.'
         return
       }
-
-      try {
-        await usersCollection.add({
-          name: values.name,
-          email: values.email,
-          age: values.age,
-          country: values.country,
-          userId: userCred.user.uid
-        })
-      } catch (error) {
-        this.reg_in_submission = false
-        this.reg_alert_variant = 'bg-red-500'
-        this.reg_alert_msg = 'An unexpected error occurred. Please try again later.'
-        return
-      }
-
-      this.userLoggedIn = true
 
       this.reg_alert_variant = 'bg-green-500'
       this.reg_alert_msg = 'Your account has been created successfully.'
--- src/stores/user.js
+++ src/stores/user.js
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia'
+import { auth, usersCollection } from '@/includes/firebase'
 
 export default defineStore('user', {
   state: () => ({
@@ -8,6 +9,15 @@ export default defineStore('user', {
     // ...
   },
   actions: {
-    // ...
+    async register(values) {
+      await auth.createUserWithEmailAndPassword(values.email, values.password),
+        await usersCollection.add({
+          name: values.name,
+          email: values.email,
+          age: values.age,
+          country: values.country
+        })
+      this.userLoggedIn = true
+    }
   }
 })
